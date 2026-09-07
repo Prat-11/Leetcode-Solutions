4c14d4267f45e3ec45618d9485d4f4856eeac864
Pratham Maini <maini.pratham2k7@gmail.com>
2026-09-08 00:02:08 +0530

    Time: 9 ms (91.08%) | Memory: 46.4 MB (86.11%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        StringBuilder st = new StringBuilder();
+
+        for (char c : num.toCharArray()) {
+
+            while (st.length() > 0 && k > 0 &&
+                   st.charAt(st.length() - 1) > c) {
+                st.deleteCharAt(st.length() - 1);
+                k--;
+            }
+
+            st.append(c);
+        }
+        while (k > 0) {
+            st.deleteCharAt(st.length() - 1);
+            k--;
+        }
+        int i = 0;
+        while (i < st.length() && st.charAt(i) == '0')
+            i++;
+
+        String ans = st.substring(i);
+
+        return ans.isEmpty() ? "0" : ans;
+    }
+}
