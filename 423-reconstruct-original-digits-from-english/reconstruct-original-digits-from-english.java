926288e200c60c39886e54d3a5c88cd092e23beb
Pratham Maini <maini.pratham2k7@gmail.com>
2026-09-22 00:57:45 +0530

    Time: 4 ms (95.32%) | Memory: 46.6 MB (22.36%) - LeetSync

---
+++ 423-reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String originalDigits(String s) {
+
+        int[] count = new int[26];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int[] num = new int[10];
+
+        num[0] = count['z' - 'a']; 
+        num[2] = count['w' - 'a']; 
+        num[4] = count['u' - 'a']; 
+        num[6] = count['x' - 'a']; 
+        num[8] = count['g' - 'a']; 
+
+        num[1] = count['o' - 'a'] - num[0] - num[2] - num[4];
+        num[3] = count['h' - 'a'] - num[8];
+        num[5] = count['f' - 'a'] - num[4];
+        num[7] = count['s' - 'a'] - num[6];
+        num[9] = count['i' - 'a'] - num[5] - num[6] - num[8];
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i <= 9; i++) {
+            while (num[i] > 0) {
+                ans.append(i);
+                num[i]--;
+            }
+        }
+
+        return ans.toString();
+    }
+}
